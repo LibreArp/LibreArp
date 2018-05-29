4c8ce447d37420c6721815711cacbabd5d8e1da6
Spiffyk <oto.stava@gmail.com>
2018-05-29 20:17:56 +0200

    Make initial preset gapless

--- Source/util/PatternUtil.cpp
+++ Source/util/PatternUtil.cpp
@@ -25,17 +25,17 @@ ArpPattern PatternUtil::createBasicPattern() {
 
     note.data.noteNumber = 0;
     note.startPoint = 0;
-    note.endPoint = 23;
+    note.endPoint = 24;
     notes.push_back(note);
 
     note.data.noteNumber = 1;
     note.startPoint = 24;
-    note.endPoint = 47;
+    note.endPoint = 48;
     notes.push_back(note);
 
     note.data.noteNumber = 2;
     note.startPoint = 48;
-    note.endPoint = 71;
+    note.endPoint = 72;
     notes.push_back(note);
 
     pattern.loopLength = 72;
