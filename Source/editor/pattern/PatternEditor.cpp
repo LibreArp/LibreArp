db53f39afc550bbc7f2ab6d9221d8572e691c36e
Spiffyk <oto.stava@gmail.com>
2018-06-18 00:15:31 +0200

    Merge branch 'hotfix/1.1' into develop

--- Source/editor/pattern/PatternEditor.cpp
+++ Source/editor/pattern/PatternEditor.cpp
@@ -49,7 +49,7 @@ PatternEditor::PatternEditor(LibreArp &p, EditorState &e, PatternEditorView *ec)
         state.lastNoteLength = processor.getPattern().getTimebase() / state.divisor;
     }
     snapEnabled = true;
-    selection = Rectangle(0, 0, 0, 0);
+    selection = Rectangle<int>(0, 0, 0, 0);
 
     setWantsKeyboardFocus(true);
 }
@@ -93,7 +93,9 @@ void PatternEditor::paint(Graphics &g) {
     auto &notes = pattern.getNotes();
     for (unsigned long i = 0; i < notes.size(); i++) {
         auto &note = notes[i];
-        Rectangle noteRect = getRectangleForNote(note);
+        Rectangle<int> noteRect = getRectangleForNote(note);
+
+        auto isPlaying = processor.getPlayingPatternIndices().contains(i);
 
         auto isPlaying = processor.getPlayingPatternIndices().contains(i);
 
@@ -298,7 +300,7 @@ void PatternEditor::mouseDown(const MouseEvent &event) {
 
 void PatternEditor::mouseUp(const MouseEvent &event) {
     setDragAction(nullptr);
-    selection = Rectangle(0, 0, 0, 0);
+    selection = Rectangle<int>(0, 0, 0, 0);
     setMouseCursor(MouseCursor::NormalCursor);
     repaint();
     Component::mouseUp(event);
@@ -515,7 +517,7 @@ void PatternEditor::moveSelectedDown() {
 
 
 void PatternEditor::select(const MouseEvent &event, PatternEditor::SelectionDragAction *dragAction) {
-    selection = Rectangle(Point(event.x, event.y), Point(dragAction->startX, dragAction->startY));
+    selection = Rectangle<int>(Point<int>(event.x, event.y), Point<int>(dragAction->startX, dragAction->startY));
 
     selectedNotes.clear();
     auto &notes = processor.getPattern().getNotes();
