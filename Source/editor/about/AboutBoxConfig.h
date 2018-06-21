c37db3c2b7987fe0a6cb4338bf50ec50921eb212
Spiffyk <oto.stava@gmail.com>
2018-06-21 23:03:36 +0200

    Add "powered by JUCE 5" to about box

--- Source/editor/about/AboutBoxConfig.h
+++ Source/editor/about/AboutBoxConfig.h
@@ -35,4 +35,7 @@
 "This program is distributed in the hope that it will be useful, " \
 "but WITHOUT ANY WARRANTY; without even the implied warranty of " \
 "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the " \
-"GNU General Public License for more details."
+"GNU General Public License for more details.\n" \
+"\n" \
+"LibreArp is powered by the JUCE 5 framework under the GNU General " \
+"Public License, version 3."
