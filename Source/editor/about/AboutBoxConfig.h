3e5f39b91ff588277aaef64f5811e75d7f6be601
Oto Šťáva <oto.stava@gmail.com>
2021-08-24 18:03:32 +0200

    Slight about box reorganization

--- Source/editor/about/AboutBoxConfig.h
+++ Source/editor/about/AboutBoxConfig.h
@@ -29,21 +29,19 @@
 #define FONT_LICENSE_URL "https://github.com/RedHatBrand/Overpass/blob/master/LICENSE.md"
 
 #define LICENSE_NOTICE juce::CharPointer_UTF8("LibreArp - A pattern-based arpeggio generator plugin\n" \
-"\n" \
 "Copyright \xc2\xa9 2019-2021 The LibreArp contributors\n" \
+"\n" \
+"Version: " LIBREARP_VERSION " (" LIBREARP_VERSION_CODE ")\n" \
+"Build time: " __DATE__ " " __TIME__ "\n" \
 "\n" \
 "LibreArp is free software: you can redistribute it and/or modify " \
 "it under the terms of the GNU General Public License as published by " \
 "the Free Software Foundation, either version 3 of the License, or " \
 "(at your option) any later version.\n" \
-"\n" \
 "LibreArp is distributed in the hope that it will be useful, " \
 "but WITHOUT ANY WARRANTY; without even the implied warranty of " \
 "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the " \
 "GNU General Public License for more details.\n" \
 "\n" \
-"\n" \
 "Powered by JUCE 6 under the GNU General Public License, version 3\n" \
-"Editor uses the Overpass font under the SIL Open Font License version 1.1\n" \
-"\n" \
-"Version: " LIBREARP_VERSION " (" LIBREARP_VERSION_CODE ")\n")
+"Editor uses the Overpass font under the SIL Open Font License version 1.1\n")
